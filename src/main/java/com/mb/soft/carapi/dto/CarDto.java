2830982600857eb5e97e54a4ed1b9a9912e46ce6
Michał Biernacki <micbiernacki@gmail.com>
2018-10-29 21:29:16 +0100

    CarDto update

--- src/main/java/com/mb/soft/carapi/dto/CarDto.java
+++ src/main/java/com/mb/soft/carapi/dto/CarDto.java
@@ -1,6 +1,25 @@
 package com.mb.soft.carapi.dto;
+import com.mb.soft.carapi.entity.CarType;
+import java.time.LocalDate;
 
 public class CarDto {
+
+    private Long id;
+    private String vin;
+    private String yearOfProduction;
+    private String brand;
+    private String model;
+    private String carType;
+
+
+    public CarDto(Long id, String vin, String yearOfProduction, String brand, String model, String carType) {
+        this.id = id;
+        this.vin = vin;
+        this.yearOfProduction = yearOfProduction;
+        this.brand = brand;
+        this.model = model;
+        this.carType = carType;
+    }
 
 
 }
