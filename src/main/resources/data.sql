82703bd6ddb19c19d1537bbe0a8e675ac64cb69f
Michał Biernacki <micbiernacki@gmail.com>
2018-10-30 21:04:20 +0100

    Dump of database of cars

---
+++ src/main/resources/data.sql
@@ -0,0 +1,59 @@
+INSERT INTO car(id, vin, year_of_production, brand, model, car_type)
+VALUES (1, '11234567899876543', '2005', 'Opel','Corsa', 'HATCHBACK');
+
+INSERT INTO car(id, vin, year_of_production, brand, model, car_type)
+VALUES (2, '98745632154795631', '2006', 'Nissan','Almera', 'SEDAN');
+
+INSERT INTO car(id, vin, year_of_production, brand, model, car_type)
+VALUES (3, '28596745236845745', '2007', 'Mercedes','A150', 'HATCHBACK');
+
+INSERT INTO car(id, vin, year_of_production, brand, model, car_type)
+VALUES (4, '12368523697451258', '2008', 'Audi','A4', 'COMBI');
+
+INSERT INTO car(id, vin, year_of_production, brand, model, car_type)
+VALUES (5, '65897978563131358', '2009', 'Volvo','V50', 'COMBI');
+
+INSERT INTO car(id, vin, year_of_production, brand, model, car_type)
+VALUES (6, '79797979797979797', '2010', 'Fiat','Bravo', 'HATCHBACK');
+
+INSERT INTO car(id, vin, year_of_production, brand, model, car_type)
+VALUES (7, '13131313131313131', '2011', 'Suzuki','Swift', 'HATCHBACK');
+
+INSERT INTO car(id, vin, year_of_production, brand, model, car_type)
+VALUES (8, '46464646464646464', '2012', 'Mazda','6', 'SEDAN');
+
+INSERT INTO car(id, vin, year_of_production, brand, model, car_type)
+VALUES (9, '58585858585858585', '2013', 'VW','Golf', 'HATCHBACK');
+
+INSERT INTO car(id, vin, year_of_production, brand, model, car_type)
+VALUES (10, '12121212121212121', '2014', 'Fiat','Tipo', 'SEDAN');
+
+INSERT INTO car(id, vin, year_of_production, brand, model, car_type)
+VALUES (11, '78787878989899889', '2015', 'Opel','Astra', 'LIFTBACK');
+
+INSERT INTO car(id, vin, year_of_production, brand, model, car_type)
+VALUES (12, '46564665656565656', '2016', 'Nissan','Qashqai', 'SUV');
+
+INSERT INTO car(id, vin, year_of_production, brand, model, car_type)
+VALUES (13, '28282828282828282', '2018', 'Opel','Corsa', 'HATCHBACK');
+
+INSERT INTO car(id, vin, year_of_production, brand, model, car_type)
+VALUES (14, '63636363636363636', '2009', 'Mazda','3', 'HATCHBACK');
+
+INSERT INTO car(id, vin, year_of_production, brand, model, car_type)
+VALUES (15, '45645645645645645', '2008', 'Audi','A6', 'SEDAN');
+
+INSERT INTO car(id, vin, year_of_production, brand, model, car_type)
+VALUES (16, '78978978978978978', '2012', 'Audi','A8', 'SEDAN');
+
+INSERT INTO car(id, vin, year_of_production, brand, model, car_type)
+VALUES (17, '36936936936936936', '2015', 'Kia','Picanto', 'HATCHBACK');
+
+INSERT INTO car(id, vin, year_of_production, brand, model, car_type)
+VALUES (18, '25825825825825825', '2016', 'Subaru','Impreza', 'SEDAN');
+
+INSERT INTO car(id, vin, year_of_production, brand, model, car_type)
+VALUES (19, '14714714714714714', '2013', 'Toyota','Yaris', 'HATCHBACK');
+
+INSERT INTO car(id, vin, year_of_production, brand, model, car_type)
+VALUES (20, '75315975315975315', '2006', 'Toyota','Corolla', 'HATCHBACK');
